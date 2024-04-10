6bcc6a05b6b062ec44b9a0d34b33fe52266ff427
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-10 15:02:23 +0000

    update

---
+++ main.py
@@ -0,0 +1,38 @@
+#Sistema Bancário 1.0
+
+#Variáveis globais
+saldo_atual = 0
+extrato = []
+
+#Funções para as operações
+def verificar_saldo():
+    print('Seu saldo atual é de {:.2f}'.format(saldo_atual))
+
+#Função para realizar saque
+def realizar_saque(amount):
+    global saldo_atual
+    if amount <= saldo_atual: #O saque deve ser menor ou igual ao saldo atual
+        saldo_atual -= amount #Saldo atual menos o saque
+        extrato.append('Saque: {:.2f}'.format(extrato))
+        print('Saque realizado com sucesso. Novo saldo {:.2f}'.format(saldo_atual))
+    else:
+        print('Saldo insuficiente.')
+
+#Função para realizar depósitos
+def realizar_deposito(amount):
+    global saldo_atual
+    saldo_atual += amount #Soma o saldo atual + depósito realizado
+    extrato.append('Depósito: {:.2f}'.format(amount))
+    print('Depósito realizado com sucesso. Novo saldo: {:.2f}'.format(saldo_atual))
+
+#Função para mostrar o histórico de transações
+def ver_historico_transacoes():
+    print('\nHistórico de Transações:')
+    for transacoes in extrato: #Cria uma variável para puxar o extrato
+        print(transacoes)
+
+#Menu principal
+
+
+
+
